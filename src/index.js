9077642fb1c95b75e02e0e42b3072f7764ceec23
Vitaly K <43697710+shut56@users.noreply.github.com>
2024-05-08 20:09:47 +0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Tiny Express Server</title>
+    <script defer>
+      fetch(/).then((r) => r.json()).then((data) => {
+            const root = document.getElementById('root')
+            root.textContent = data            
+      })
+    </script>
+  </head>
+<body>
+  <div id="root"></div>
+</body>
+</html>
